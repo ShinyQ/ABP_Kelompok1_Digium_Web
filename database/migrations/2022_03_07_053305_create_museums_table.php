97b883ba6e75d58ae8056f8a48a3bebb58fbb4a1
Kurniadi Ahmad Wijaya <kurniadiahmadwijaya@gmail.com>
2022-03-08 23:17:20 +0700

    Update 2022_03_07_053305_create_museums_table.php

--- database/migrations/2022_03_07_053305_create_museums_table.php
+++ database/migrations/2022_03_07_053305_create_museums_table.php
@@ -15,12 +15,12 @@ return new class extends Migration
     {
         Schema::create('museums', function (Blueprint $table) {
             $table->id();
-            $table->string('nama', 100);
-            $table->string('background', 100);
-            $table->string('panorama', 100);
+            $table->string('name');
+            $table->string('background');
+            $table->string('panorama');
             $table->text('description');
-            $table->string('location', 100);
-            $table->string('phone', 100);
+            $table->string('address');
+            $table->string('phone');
             $table->date('year_built')->nullable();
             $table->point('coordinate');
             $table->integer('price')->nullable();
