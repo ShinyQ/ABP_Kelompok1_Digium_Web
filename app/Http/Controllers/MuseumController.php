a773a245631c1b8daf355df59886d18787f12c54
Imam Rafiif Arrazaan <karrazaan@gmail.com>
2022-04-14 05:15:12 +0700

    CRUD museum
    adding CRUD feature to museum class. adding "tambah data" button in museum.index view. adjust museum model fillable field to match with museum field in migration.

--- app/Http/Controllers/MuseumController.php
+++ app/Http/Controllers/MuseumController.php
@@ -17,12 +17,28 @@ class MuseumController extends Controller
 
     public function create()
     {
-        //
+        $title = 'Add Museum';
+        $method = 'post';
+        $action = 'museum';
+        return view('museum.add', compact('title', 'method', 'action'));
     }
 
     public function store(Request $request)
     {
-        //
+        $museum = Museum::create([
+            'name' => $request->name,
+            'background' => $request->background,
+            'panorama' => $request->panorama,
+            'description' => $request->description,
+            'address' => $request->address,
+            'phone' => $request->phone,
+            'year_built' => $request->year_built,
+            'latitude' => $request->latitude,
+            'longitude' => $request->longitude, 
+            'price' => $request->price,
+        ]);
+        return redirect('/museum')->with('msg', 'Data museum berhasil ditambahkan');
+
     }
 
     public function show($id)
@@ -33,16 +49,32 @@ class MuseumController extends Controller
 
     public function edit($id)
     {
-        //
+        $title = 'Edit Museum';
+        $data = Museum::find($id);
+        return view('museum.update', compact('title', 'data'));
     }
 
     public function update(Request $request, $id)
     {
-        //
+        Museum::where('id', $id)
+        ->update([
+            'name' => $request->name,
+            'background' => $request->background,
+            'panorama' => $request->panorama,
+            'description' => $request->description,
+            'address' => $request->address,
+            'phone' => $request->phone,
+            'year_built' => $request->year_built,
+            'latitude' => $request->latitude,
+            'longitude' => $request->longitude, 
+            'price' => $request->price,
+        ]);
+        return redirect('/museum')->with('msg', 'Data museum berhasil diperbarui');
     }
 
     public function destroy($id)
     {
-        //
+        Museum::destroy($id);
+        return redirect('/museum')->with('msg', 'Data museum berhasil dihapus');
     }
 }
