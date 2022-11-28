ecffa81192c21cc88cae155fdc5c04e4c8429d7a
Michael Putera Wardana <60507035+krobus00@users.noreply.github.com>
2022-11-28 22:49:33 +0700

    Update UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -23,7 +23,7 @@ class UserSeeder extends Seeder
                 'name' => "ADMIN",
                 'email' => "admin@gmail.com",
                 'email_verified_at' => now(),
-                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+                'password' => bcrypt('admin'),
                 'remember_token' => Str::random(10),
                 'role' => "superuser"
             ],
